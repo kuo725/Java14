529001eec6c6eb074a1dc3ec09ffae26d7c2e124
kuo725 <113486769+kuo725@users.noreply.github.com>
2022-12-24 23:16:02 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,15 @@
+package myJava14;
+
+import java.util.Scanner;
+import java.lang.String;
+
+public class Class02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str;
+		System.out.print("¿é¤J¦r¦ê:");
+		str = sc.nextLine();
+		System.out.println("Âà´«¦¨¤j¼g:" + str.toUpperCase());
+	}
+}
